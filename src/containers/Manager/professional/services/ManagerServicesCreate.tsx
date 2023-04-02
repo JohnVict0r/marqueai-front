69a08e64e71acf6e9ef772da8e767dca63f0acb6
johnvict0r <johnvictorio3@gmail.com>
2023-04-02 11:13:15 -0300

    feat: services

--- src/containers/Manager/professional/services/ManagerServicesCreate.tsx
+++ src/containers/Manager/professional/services/ManagerServicesCreate.tsx
@@ -1,4 +1,4 @@
-import React, { useState} from 'react'
+import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import {
   Form,
@@ -8,6 +8,8 @@ import {
   Radio,
   Row,
   Col,
+  InputNumber,
+  message,
 } from 'antd'
 
 import Panel from '../../../../components/Panel'
@@ -30,23 +32,26 @@ const validateMessages = {
   },
 }
 
-function ManagerPuzzleItemCreate(){
-  const params = useParams<any>()
+function ManagerPuzzleItemCreate() {
   const [form] = Form.useForm()
-  const [controlable, setControlable] = useState(false)
   const [loading, setLoading] = useState(false)
+  const [messageApi, contextHolder] = message.useMessage()
 
- const onFinish = (values: any) => {
+  const onFinish = (values: any) => {
     setLoading(true)
     console.log(values)
     api
-      .post(`/puzzles/${params.puzzle_id}/items`, {
+      .post(`/services`, {
         ...values,
-        controlable,
       })
       .then(() => {
         setLoading(false)
         form.resetFields()
+        messageApi.open({
+          type: 'success',
+          content: 'Serviço cadastrado com sucesso!',
+          duration: 3,
+        })
       })
       .catch(({ response }) => {
         setLoading(false)
@@ -61,173 +66,171 @@ function ManagerPuzzleItemCreate(){
       })
   }
 
-  return(
-  <>
-  <Panel title='Cadastrar Item'>
-      <Row gutter={[24, 24]} style={{ width: '100%' }}>
-        <Col xs={{ span: 24 }} lg={{ span: 12, offset: 6 }}>
-      <Form
-        name='login-form'
-        className='login-form'
-        layout='vertical'
-        form={form}
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        onFinishFailed={err => console.log('erro no finish', err)}
-        requiredMark={false}
-        scrollToFirstError
-        validateMessages={validateMessages}
-      >
-        <Form.Item
-          label='Nome de Cadastro'
-          name='name'
-          className='login-form-item'
-          rules={[
-            {
-              required: true,
-              message: 'Por favor, insira o Nome do Item!',
-            },
-            {
-              pattern: new RegExp('[a-zA-Z \u00C0-\u00FF]'),
-              message: 'Apenas letras!',
-            },
-          ]}
-        >
-          <Input
-            placeholder='Ex.: Exames'
-            size='large'
-            onKeyPress={e => {
-              const specialCharRegex = new RegExp('[a-zA-Z \u00C0-\u00FF]')
-              const pressedKey = String.fromCharCode(
-                !e.charCode ? e.which : e.charCode
-              )
-              if (!specialCharRegex.test(pressedKey)) {
-                e.preventDefault()
-                return false
-              }
-              return true
-            }}
-          />
-        </Form.Item>
+  return (
+    <>
+      {contextHolder}
+      <Panel title='Cadastrar Serviço'>
+        <Row gutter={[24, 24]} style={{ width: '100%' }}>
+          <Col xs={{ span: 24 }} lg={{ span: 12, offset: 6 }}>
+            <Form
+              name='login-form'
+              className='login-form'
+              layout='vertical'
+              form={form}
+              onFinish={onFinish}
+              onFinishFailed={err => console.log('erro no finish', err)}
+              requiredMark={false}
+              scrollToFirstError
+              validateMessages={validateMessages}
+            >
+              <Form.Item
+                label='Nome'
+                name='name'
+                className='login-form-item'
+                rules={[
+                  {
+                    required: true,
+                    message: 'Por favor, insira o Nome do Serviço!',
+                  },
+                  {
+                    pattern: new RegExp('[a-zA-Z \u00C0-\u00FF]'),
+                    message: 'Apenas letras!',
+                  },
+                ]}
+              >
+                <Input
+                  placeholder='Ex.: Corte de cabelo'
+                  size='large'
+                  onKeyPress={e => {
+                    const specialCharRegex = new RegExp(
+                      '[a-zA-Z \u00C0-\u00FF]'
+                    )
+                    const pressedKey = String.fromCharCode(
+                      !e.charCode ? e.which : e.charCode
+                    )
+                    if (!specialCharRegex.test(pressedKey)) {
+                      e.preventDefault()
+                      return false
+                    }
+                    return true
+                  }}
+                />
+              </Form.Item>
 
-        <Form.Item
-          label='Nome de exibição'
-          name='display_name'
-          className='login-form-item'
-          rules={[
-            {
-              required: true,
-              message: 'Por favor, insira o Nome de exibição do Item!',
-            },
-            {
-              pattern: new RegExp('[a-zA-Z \u00C0-\u00FF]'),
-              message: 'Apenas letras!',
-            },
-          ]}
-        >
-          <Input
-            placeholder='Ex.: Exames'
-            size='large'
-            onKeyPress={e => {
-              const specialCharRegex = new RegExp('[a-zA-Z \u00C0-\u00FF]')
-              const pressedKey = String.fromCharCode(
-                !e.charCode ? e.which : e.charCode
-              )
-              if (!specialCharRegex.test(pressedKey)) {
-                e.preventDefault()
-                return false
-              }
-              return true
-            }}
-          />
-        </Form.Item>
+              <Form.Item
+                label='Descrição'
+                name='description'
+                className='login-form-item'
+                rules={[
+                  {
+                    required: true,
+                    message: 'Por favor, insira a descrição do serviço!',
+                  },
+                  {
+                    pattern: new RegExp('[a-zA-Z \u00C0-\u00FF]'),
+                    message: 'Apenas letras!',
+                  },
+                ]}
+              >
+                <Input
+                  placeholder='Ex.: Corte de cabelo na tesoura...'
+                  size='large'
+                  onKeyPress={e => {
+                    const specialCharRegex = new RegExp(
+                      '[a-zA-Z \u00C0-\u00FF]'
+                    )
+                    const pressedKey = String.fromCharCode(
+                      !e.charCode ? e.which : e.charCode
+                    )
+                    if (!specialCharRegex.test(pressedKey)) {
+                      e.preventDefault()
+                      return false
+                    }
+                    return true
+                  }}
+                />
+              </Form.Item>
 
-        <Form.Item
-          label='Descrição'
-          name='description'
-          className='login-form-item'
-          rules={[
-            {
-              required: true,
-              message: 'Por favor, insira o Nome de exibição do Item!',
-            },
-            {
-              pattern: new RegExp('[a-zA-Z \u00C0-\u00FF]'),
-              message: 'Apenas letras!',
-            },
-          ]}
-        >
-          <Input
-            placeholder='Ex.: Exames'
-            size='large'
-            onKeyPress={e => {
-              const specialCharRegex = new RegExp('[a-zA-Z \u00C0-\u00FF]')
-              const pressedKey = String.fromCharCode(
-                !e.charCode ? e.which : e.charCode
-              )
-              if (!specialCharRegex.test(pressedKey)) {
-                e.preventDefault()
-                return false
-              }
-              return true
-            }}
-          />
-        </Form.Item>
+              <Form.Item
+                label='Preço'
+                name='price'
+                className='login-form-item'
+                rules={[
+                  {
+                    required: true,
+                    message: 'Por favor, insira o preço do serviço!',
+                  },
+                ]}
+              >
+                <InputNumber
+                  placeholder='R$20,00'
+                  size='large'
+                  onKeyPress={e => {
+                    const specialCharRegex = new RegExp(
+                      '[a-zA-Z \u00C0-\u00FF]'
+                    )
+                    const pressedKey = String.fromCharCode(
+                      !e.charCode ? e.which : e.charCode
+                    )
+                    if (!specialCharRegex.test(pressedKey)) {
+                      e.preventDefault()
+                      return false
+                    }
+                    return true
+                  }}
+                />
+              </Form.Item>
 
-        <Form.Item
-          name='controlable'
-          className='login-form-item'
-          label={
-            <span>
-              Este Item possui Estoque?*{' '}
-              <Tooltip title='Precisamos que informe se o seu Item possui algum estoque inicial'>
-                <QuestionCircleOutlined />
-              </Tooltip>
-            </span>
-          }
-          rules={[
-            {
-              required: true,
-              message: 'Por favor, informe se seu item possui estoque!',
-            },
-          ]}
-        >
-          <Radio.Group>
-            <Radio onClick={() => setControlable(true)} value={true}>Sim</Radio>
-            <Radio onClick={() => setControlable(false)} value={false}>Não</Radio>
-          </Radio.Group>
-
-        </Form.Item>
-
-        {controlable &&
-           <Form.Item
-           label='Quantidade em Estoque'
-           className='login-form-item'
-           name='controlable_amount'
-           >
-             <Input
-               placeholder=''
-               size='large'
-               maxLength={9}
-             />
-         </Form.Item>
-        }
-        <Form.Item className='login-form-item'>
-          <Button
-            type='primary'
-            htmlType='submit'
-            className='login-form-button'
-            size='large'
-            loading={loading}
-            style={{ height: `48px`, fontSize: `16px`, fontWeight: `bold` }}
-          >
-            Cadastrar Item
-          </Button>
-        </Form.Item>
-      </Form>
-      </Col>
-      </Row>
-    </Panel>
+              <Form.Item
+                label='Tempo de duração'
+                name='duration'
+                className='login-form-item'
+                rules={[
+                  {
+                    required: true,
+                    message:
+                      'Por favor, insira o tempo de duração do serviço em minutos!',
+                  },
+                ]}
+              >
+                <InputNumber
+                  placeholder='30min'
+                  size='large'
+                  onKeyPress={e => {
+                    const specialCharRegex = new RegExp(
+                      '[a-zA-Z \u00C0-\u00FF]'
+                    )
+                    const pressedKey = String.fromCharCode(
+                      !e.charCode ? e.which : e.charCode
+                    )
+                    if (!specialCharRegex.test(pressedKey)) {
+                      e.preventDefault()
+                      return false
+                    }
+                    return true
+                  }}
+                />
+              </Form.Item>
+              <Form.Item className='login-form-item'>
+                <Button
+                  type='primary'
+                  htmlType='submit'
+                  className='login-form-button'
+                  size='large'
+                  loading={loading}
+                  style={{
+                    height: `48px`,
+                    fontSize: `16px`,
+                    fontWeight: `bold`,
+                  }}
+                >
+                  Cadastrar Item
+                </Button>
+              </Form.Item>
+            </Form>
+          </Col>
+        </Row>
+      </Panel>
     </>
   )
 }
--- src/containers/Manager/professional/services/ManagerServicesList.tsx
+++ src/containers/Manager/professional/services/ManagerServicesList.tsx
@@ -1,156 +1,140 @@
-import React, { useEffect,useState, useMemo } from 'react'
+import React, { useEffect, useState, useMemo } from 'react'
 import Panel from '../../../../components/Panel'
-import { Row, Col,  List, Statistic,Button, Card  } from 'antd'
-import {PushpinOutlined, FormOutlined } from '@ant-design/icons'
+import { Row, Col, List, Button, Card } from 'antd'
+import { PushpinOutlined, FormOutlined } from '@ant-design/icons'
 import { useHistory, useParams } from 'react-router-dom'
 import api from '../../../../services/api'
-import "@progress/kendo-theme-material/dist/all.css";
-import "hammerjs";
-import Donut from '../../../../components/Charts/Donut'
+import '@progress/kendo-theme-material/dist/all.css'
+import 'hammerjs'
 
 function PuzzleItemList() {
   const isMobile = window.innerWidth < 720
   const params = useParams<any>()
   const history = useHistory()
   const [dataItem, setDataItem] = useState<any[]>([])
-  const [dataPuzzle, setDataPuzzle] = useState<any[]>([])
-
-  const currentPuzzle = useMemo<any>(() => {
-    return dataPuzzle.filter(item => item.id === Number(params.puzzle_id))[0]
-    //eslint-disable-next-line
-  }, [dataPuzzle])
-
 
   useEffect(() => {
-    api.get(`/puzzles/${params.puzzle_id}/items`).then((response) => {
-      setDataItem(response.data)
+    api.get(`/services`).then(response => {
+      setDataItem(response.data.data)
     })
   }, [params.puzzle_id])
 
-  useEffect(() => {
-    api.get(`/puzzles`).then((response) => {
-      setDataPuzzle(response.data)
-    })
-
-  }, [])
-
-  //console.log(actualPuzzle)
-   /*  const time_format = (time:any) =>{
-      let hours = adicionaZero(Math.trunc(time/60))
-      let minutes = adicionaZero(time%60)
-
-      return `${hours}h${minutes}min`
-    }
-
-    function adicionaZero(numero:any) {
-      if (numero <= 9)
-        return "0" + numero;
-      else
-        return numero;
-    } */
-
-  /* const onlyUnique = (value:any, index:any, self:any) => {
-    return self.indexOf(value) === index;
+  const priceFormat = (price: any) => {
+    return `R$${price},00`
   }
 
-  const get_unique_date = (data:any) => {
-    let atribute = data.map((item:any)=>item.user_name);
-    let unique = atribute.filter(onlyUnique);
+  const timeFormat = (time: any) => {
+    const minutesCalc = time % 60
+    const hourCalc = Math.trunc(time / 60)
 
-    return unique;
-  } */
+    if (!hourCalc) return `${minutesCalc}min`
+    if (!minutesCalc) return `${hourCalc}h`
+
+    const minutes = adicionaZero(time % 60)
+
+    return `${hourCalc}h${minutes}min`
+  }
+
+  function adicionaZero(numero: any) {
+    if (numero <= 9) return '0' + numero
+    else return numero
+  }
 
   return (
-
     <>
-
       <Panel
-        title='Controle de Itens'
-         action={
+        title='Serviços cadastrados'
+        action={
           <Button
             style={{ backgroundColor: '#1e3978', color: '#FCFCFC' }}
             icon={<FormOutlined />}
             onClick={() => {
-              history.push(`/gestao/modulos/${params.puzzle_id}/itens/cadastro`)
+              history.push(`/manager/professional/services/create`)
             }}
           >
-            Cadastrar Item
-      </Button>
-
+            Cadastrar Serviço
+          </Button>
         }
       >
         <Row gutter={[24, 24]} style={{ width: '100%' }}>
           <Col xs={{ span: 24 }} lg={{ span: 24 }}>
-            <Statistic title="Itens cadastrados" value={dataItem.length} />
-
-          </Col>
-
-          <Col xs={{ span: 24 }} lg={{ span: 24 }}>
-          <Panel title={'últimos cadastrados'}
-
-          /* action={
-            <Button
-              style={{ backgroundColor: '#1e3978', color: '#FCFCFC' }}
-              icon={<FormOutlined />}
-              onClick={() => {
-                history.push('/gestao/modulos/cadastro')
-              }}
-            >
-              Listar Todos
-            </Button>
-          } */
-          >
-              {!isMobile ? <List
-                grid={{
-                  gutter: 16,
-                  xs: 24,
-                  lg: 4,
-                  column: 4
-                }}
-                style = {{width:'95%' }}
-                dataSource={dataItem}
-                renderItem={item => (
-                  <List.Item key={item.id}>
-                    <Card style = {{padding:'0'}} title={<p> <PushpinOutlined /> <strong>{item.display_name}</strong> </p>}>
-                      {[<p> <strong>Descrição:</strong>  {item.description} </p>,
-                      <p> <strong> Estoque inicial:</strong> {item.controlable && item.controlable_amount}</p>,
-                      ]}
-                    </Card>
-                  </List.Item>
-                )}
-              />: <List
-              dataSource={dataItem}
-              renderItem={item => (
-                <List.Item>
-                  <Card title={<p> <PushpinOutlined /> <strong>{item.display_name}</strong> </p>}>
-                    {[<p> <strong>Descrição:</strong>  {item.description} </p>,
-                    <p> <strong> Estoque inicial:</strong> {item.controlable && item.controlable_amount}</p>,
-                    ]}
-                  </Card>
-                </List.Item>
+            <Panel title=''>
+              {!isMobile ? (
+                <List
+                  grid={{
+                    gutter: 16,
+                    xs: 24,
+                    lg: 4,
+                    column: 4,
+                  }}
+                  style={{ width: '95%' }}
+                  dataSource={dataItem}
+                  renderItem={item => (
+                    <List.Item key={item.id}>
+                      <Card
+                        style={{ padding: '0' }}
+                        title={
+                          <p>
+                            {' '}
+                            <PushpinOutlined /> <strong>
+                              {item.name}
+                            </strong>{' '}
+                          </p>
+                        }
+                      >
+                        {[
+                          <p>
+                            {' '}
+                            <strong>Descrição:</strong> {item.description}{' '}
+                          </p>,
+                          <p>
+                            {' '}
+                            <strong> Preço:</strong> {priceFormat(item.price)}
+                          </p>,
+                          <p>
+                            {' '}
+                            <strong> Duração:</strong>{' '}
+                            {timeFormat(item.duration)}
+                          </p>,
+                        ]}
+                      </Card>
+                    </List.Item>
+                  )}
+                />
+              ) : (
+                <List
+                  dataSource={dataItem}
+                  renderItem={item => (
+                    <List.Item>
+                      <Card
+                        title={
+                          <p>
+                            {' '}
+                            <PushpinOutlined />{' '}
+                            <strong>{item.display_name}</strong>{' '}
+                          </p>
+                        }
+                      >
+                        {[
+                          <p>
+                            {' '}
+                            <strong>Descrição:</strong> {item.description}{' '}
+                          </p>,
+                          <p>
+                            {' '}
+                            <strong> Estoque inicial:</strong>{' '}
+                            {item.controlable && item.controlable_amount}
+                          </p>,
+                        ]}
+                      </Card>
+                    </List.Item>
+                  )}
+                />
               )}
-            />}
-          </Panel>
-          </Col>
-
-
-
-       {currentPuzzle && currentPuzzle.chartable===true &&
-
-          <Col xs={{ span: 24 }} lg={{ span: 24 }}>
-            <Panel title={'Análise dos Itens'}>
-            {dataItem.length>0 &&
-              <Donut
-                title={'Porcentagem de estoque dos itens'}
-                data={dataItem}
-              />
-            }
             </Panel>
           </Col>
-        }
         </Row>
       </Panel>
-
     </>
   )
 }
