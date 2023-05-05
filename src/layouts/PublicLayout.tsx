3ea7a6d60185ad556743bfa466216fcf3d52a7ee
johnvict0r <johnvictorio3@gmail.com>
2023-05-04 21:08:07 -0300

    fix: resolve login page

--- src/layouts/PublicLayout.tsx
+++ src/layouts/PublicLayout.tsx
@@ -1,26 +1,15 @@
 import React, { FC } from 'react'
-import Logo from '../assets/marqueai.png'
 
-import { Layout, Row, Col, Image } from 'antd'
+import { Layout, Row, Col } from 'antd'
 
 const PublicLayout: FC = ({ children }) => {
   return (
     <Layout style={{ backgroundColor: '#FCFCFC', height: '100vh' }}>
-      <div
-        style={{
-          // paddingTop: '20px',
-          // paddingBottom: '10px',
-          textAlign: 'center',
-          height: '20vh',
-        }}
-      >
-        <Image src={Logo} width='240px' />
-      </div>
       <Row
         justify='center'
         align='middle'
         style={{
-          height: '80vh',
+          height: '100vh',
         }}
       >
         <Col xs={{ span: 20 }} lg={{ span: 8 }}>
