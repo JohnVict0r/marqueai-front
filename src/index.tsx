9be0ed6849f3693e98a291a2f96667b599ae5136
johnvict0r <johnvictorio3@gmail.com>
2023-08-11 14:43:41 -0300

    fix: register serviceWork

--- src/index.tsx
+++ src/index.tsx
@@ -13,5 +13,15 @@ ReactDOM.render(
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+// Learn more about service workers: https://cra.link/PWA
+serviceWorker.register({
+  onUpdate: e => {
+    const { waiting: { postMessage = null } = {} as any, update } = e || {}
+    if (postMessage) {
+      postMessage({ type: 'SKIP_WAITING' })
+    }
+    update().then(() => {
+      window.location.reload()
+    })
+  },
+})
