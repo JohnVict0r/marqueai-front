7c8c0a78c71b88a7bd856a305ed4319310b229ea
johnvict0r <johnvictorio3@gmail.com>
2023-08-24 12:42:32 -0300

    fix: cache clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect } from 'react'
 import './App.less'
 import AppProvider from './contexts'
 
 import Routes from './routes'
 
 const App: FC = () => {
+  useEffect(() => {
+    caches.keys().then(names => {
+      names.forEach(name => {
+        caches.delete(name)
+      })
+    })
+    console.log('cache clean')
+  }, [])
+
   return (
     <AppProvider>
       <Routes />
